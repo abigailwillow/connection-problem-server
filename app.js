a89d2a0432b383e3ab19d4485e0af5ed36d5009b
Abigail de Joode <abbydiode@gmail.com>
2021-07-05 13:55:24 +0200

    Add checks for failed queries

--- app.js
+++ app.js
@@ -12,27 +12,43 @@ app.use(express.json());
 
 app.use((request, response, next) => {
     if (request.headers.authorization !== `Basic ${TOKEN}`) {
-        response.status(403).json({message: 'No valid authorization token supplied'})
+        response.status(403).json({message: 'No valid authorization token supplied'});
     }
     next();
 });
 
 app.get('/scores', (request, response) => {
-    db.getScores(request.query.limit, leaderboard => response.json(leaderboard));
+    db.getScores(request.query.limit, leaderboard => {
+        if (leaderboard) {
+            response.status(200);
+            response.json(leaderboard);
+        } else {
+            response.status(404);
+            response.json({message: "Could not get scores"});
+        }
+    });
 });
 
 app.get('/score/:steamid', (request, response) => {
-    db.getScore(request.params.steamid, (steamid, name, score) => response.json({steamid: steamid, name: name, score: score}))
+    db.getScore(request.params.steamid, (steamid, name, score) => {
+        if (steamid) {
+            response.status(200);
+            response.json({steamid: steamid, name: name, score: score});
+        } else {
+            response.status(404);
+            response.json({message: `Could not get score for SteamID ${request.params.steamid}`});
+        } 
+    });
 });
 
 app.post('/score/:steamid', (request, response) => {
-    let steamid = request.body.steamid;
-    db.upsertScore(steamid, request.body.name, request.body.score, err => {
+    db.upsertScore(request.body.steamid, request.body.name, request.body.score, err => {
         if (!err) {
-            response.json({message: `Set score for ${steamid} to ${request.body.score}`});
+            response.status(200);
+            response.json({steamid: request.body.steamid, name: request.body.name, score: request.body.score});
         } else {
-            response.status(500);
-            response.json({message: `Unable to update score for ${steamid}`, error: err});
+            response.status(404);
+            response.json({message: `Unable to update score for ${request.body.name}`, error: err});
         }
     });
 });
